0bf7600dcdc8476e1d33ea64a302c92bbe1cb7fe
Hitesh <hiteshdabur12@gmail.com>
2023-10-06 22:45:42 +0530

    Commit message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+
+import React from 'react';
+import {BrowserRouter as Router,Routes , Route, Navigate } from "react-router-dom"; 
 import './App.css';
+import Navbar from './components/navbar';
+import signin from "./assets/Signin.png"
+import trade from "./assets/Trading-screen.png"
+
+function Home() {
+  return(
+    <div>
+      <div className="img-container">
+            <div className="signin">
+              <img src= {signin} />
+              <div className="bs">
+                <button className='b1'>Trade Crypto</button>
+                <button className='b1'>Sign In</button>
+              </div>
+              </div>
+
+      <div className="trade">
+        <img src={trade}/>
+     </div>
+
+     </div>
+     </div>
+
+  ) 
+};
+
+// Create a KnowMore component for the "know more" page
+function KnowMore() {
+  return <h1>Know More Page</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/know-more" element={<KnowMore />} />
+          {/* <Route exact path="/governance" component={Governance} />
+          <Route exact path="/community" component={Community} />
+          <Route exact path="/developers" component={Developers} />
+          <Route exact path="/neuron" component={Neuron} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import "./navbar.css"
+function Navbar() {
+    return (
+     
+        <div className="nav">
+          <div className="w-1/2">
+
+          </div>
+          <div href="#" className="p-0 m-0">
+            know More
+          </div>
+          <div href="#">
+           Neucron
+          </div>
+          <div href="#">
+            Governance
+          </div>
+          <div href="#">
+           Community
+          </div>
+          <div href="#">
+          Developers
+          </div>
+          
+        </div>
+    );
+  }
+  
+  export default Navbar;
